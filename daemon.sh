10b77cadd09afeae9f36d2ab044ad0212d09383c
quentltl <quentin.letellier@utt.fr>
2020-12-29 13:05:42 +0100

    reflexion

--- daemon.sh
+++ daemon.sh
@@ -1,35 +1,75 @@
 #!/bin/bash
 
+#variable couleur pour la mise en page du fichier log
 blanc='\e[1;37m'
 rougefonce='\e[0;31m'
 vertfonce='\e[0;32m'
 orange='\e[0;33m'
 violetclair='\e[1;35m'
+#-------------------------------------------------------
 
-nom_du_jour_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f1)
-jour_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f2)
-mois_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f3)
-heure_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f4)
-minute_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f5)
-seconde_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f6)
-if [ $seconde_reel -eq 0 ]; 
-then
-        interval_seconde_reel = 0
-elif [ $seconde_reel -gt 0 ] && [ $seconde_reel -le 15 ];
-then 
-        interval_seconde_reel = 1
-elif [ $seconde_reel -gt 15 ] && [ $seconde_reel -ls 30 ];
-then    
-        interval_seconde_reel = 2
-else
-        interval_seconde_reel = 3
-fi
 
-date_reel= $(date +%u-%d-%m-%H-%M-%S)
- 
-
-cat /etc/tacherontab | while read user_programmeur seconde_virtuel minute_virtuel heure_virtuel jour_virtuel mois_virtuel nom_du_jour_virtuel commande
+#boucle sans fin (à voir comment gérer en rendant le service demons)
+while [ 0 = 0]
 do
+
+while read user
+        #variable pour la date et heure système réelle
+        nom_du_jour_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f1)
+        jour_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f2)
+        mois_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f3)
+        heure_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f4)
+        minute_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f5)
+        seconde_reel = $(date +%u-%d-%m-%H-%M-%S | cut -d'-' -f6)
+        date_reel= $(date +%u-%d-%m-%H-%M-%S)
+        #-------------------------------------------------------
+
+        #on boucle sur le fichier tacherontab pour lire les commandes
+        cat /etc/tacherontab | while read user_programmeur seconde_virtuel minute_virtuel heure_virtuel jour_virtuel mois_virtuel nom_du_jour_virtuel commande
+        do
+        execution_commande = "faux"
+        #on teste si le user qui a indiqué la commande a été banni entre deux
+        if ( grep "$user_programmeur" "/etc/tacheron.allow" ) || [ "$(whoami)" == "root" ]
+        then
+                #test interval seconde réelle
+                if [ $seconde_reel -eq 0 ]; 
+                then
+                        interval_seconde_reel = 0
+                elif [ $seconde_reel -gt 0 ] && [ $seconde_reel -le 15 ];
+                then 
+                        interval_seconde_reel = 1
+                elif [ $seconde_reel -gt 15 ] && [ $seconde_reel -ls 30 ];
+                then    
+                        interval_seconde_reel = 2
+                else
+                        interval_seconde_reel = 3
+                fi
+
+                #on test l'interval des secondes
+                if [ "$seconde_virtuel" != "*" ];
+                then
+                        if [ "$seconde_virtuel" = "$interval_seconde_reel" ] ;
+                        then
+                                continue #on continue de tester les autres lignes de tacherontab (continue s'applique au while)
+                        fi
+                fi
+        
+        else
+                echo "L'utilisateur qui a inscrit cette commande a été banni";
+                execution_commande = "faux"
+        fi
+
+        if [ "$execution_commande" = ""]
+
+
+
+        done < /etc/tacherontab #lecture des commandes de tacherontab
+
+done < /etc/tacheron.allow #lecture des users autorisés
+
+done
+
+
 
         if [[ $mois_virtuel = ^[1-12]$ ]]; #format valeur précis
         then
